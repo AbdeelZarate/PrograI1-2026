146fa66eb7d63e8d36b2ff10e29e7faf67494bb7
AbdeelZarate <abdeel.zarate@ucb.edu.bo>
2026-02-18 07:51:07 -0400

    Ejercicio_01_03.cpp

---
+++ PRACTICA_01/Ejercicio_01_03.cpp
@@ -0,0 +1,24 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Abdeel Ricardo Zárate Calle
+// Carnet: 13024920.
+// Carrera del estudiante: ingenieria BIomedica.
+// Fecha creación: 18/02/2026
+
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    system("cls");
+    int edad=0;//tipo entero//
+    char sexo;///tipo caracter/
+    float altura=0;//tipo real//
+    
+    cout<<"Edad: ";
+    cin>>edad;
+    cout<<"Altura en metros: ";
+    cin>>altura;
+    cout<<"Sexo: ";
+    cin>>sexo;
+    return 0;
+}
